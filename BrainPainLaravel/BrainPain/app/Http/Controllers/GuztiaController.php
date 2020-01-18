8b53016f94c1285240a3cf92e1d103ca3c6e5add
Xabier <xbca97@gmail.com>
2020-01-18 13:08:59 +0100

    Revert "fuck"
    This reverts commit d9bae9ea463bddaba3818894dc43aecba2825e79.

--- BrainPainLaravel/BrainPain/app/Http/Controllers/GuztiaController.php
+++ BrainPainLaravel/BrainPain/app/Http/Controllers/GuztiaController.php
@@ -98,6 +98,5 @@ class GuztiaController extends Controller
         $postdata = file_get_contents("php://input");
         if (isset($postdata)) {
             $request = json_decode($postdata);
-        }
     }
 }

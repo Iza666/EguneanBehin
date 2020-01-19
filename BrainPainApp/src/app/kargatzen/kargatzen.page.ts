5466496250670fec8ef9159e8061ee49c046a066
Iza666 <0izugar0@gmail.com>
2020-01-19 22:40:27 +0100

    Loading screen with timer

--- BrainPainApp/src/app/kargatzen/kargatzen.page.ts
+++ BrainPainApp/src/app/kargatzen/kargatzen.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { timer } from 'rxjs/observable/timer';
 
 @Component({
   selector: 'app-kargatzen',
@@ -7,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KargatzenPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    timer(3000).subscribe( x =>{
+      this.router.navigate(['/galdera']);
+    });
   }
-
 }
